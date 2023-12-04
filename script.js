1f0b8c767da971280664a2d5678c5ae90af3620a
code-with-rudra <136544117+code-with-rudra@users.noreply.github.com>
2023-12-04 16:03:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let img = document.getElementById('img');
+ function butOff(){
+    img.src = 'bulboff.png' ;   
+
+ }
+ function butOn(){
+    let body = document.getElementsByTagName('body');
+    img.src = 'Bulbon.png';
+   
+ }
